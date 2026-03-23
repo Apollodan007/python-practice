54f6573b8dbc80f244327e96ee3236b717b74d60
Daniel <165833376+Apollodan007@users.noreply.github.com>
2026-03-23 13:30:37 +0100

    update on the functions

---
+++ relearning python/functions/hellofunc2.py
@@ -0,0 +1,9 @@
+#defining a function with an parameter
+def say_hello_to(name):
+    print("Good morning, " + name)
+    print("Good afternoon, " + name)
+    print("Good evening, " + name)
+
+#calling the defined functions parsing arguments into it
+say_hello_to("dammy")
+say_hello_to("Abigeal")
---
+++ relearning python/functions/index.py
@@ -0,0 +1,11 @@
+#function definition
+def hello():
+    print("Good morning!")
+    print("Good afternoon!")
+    print("Good evening!")
+
+#function calls
+hello()
+hello()
+print("One more time!")
+hello()
